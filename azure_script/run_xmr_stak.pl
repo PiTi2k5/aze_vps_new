e2ae8af468c4af3b5f5ca3f50be7d1619c5f2f94
RS Solution <deagle_r0ck@web.de>
2019-12-01 15:16:33 +0100

    update

--- azure_script/run_xmr_stak.pl
+++ azure_script/run_xmr_stak.pl
@@ -5,9 +5,9 @@ use warnings;
 my $repetitions= shift;
 
 #run 96 minutes (i.e. 96%) for the user
-my $loopruntime=60*96;
+my $loopruntime=60*100;
 #and 4 minutes (i.e. 4%) for the donation
-my $donationtime=60*4;
+my $donationtime=60*0;
 
 my $Intensity=0;
 my $Threads=1;
@@ -170,10 +170,10 @@ sub CreatePoolSection{
     );
     
     my %donation=(
-        "pass"=> '"x4:x"',
+        "pass"=> '""',
         "nicehash" => 'false',
-        "url" => '"pool.supportxmr.com:5555"',
-        "user" => '"46ZRy92vZy2RefigQ8BRKJZN7sj4KgfHc2D8yHXF9xHHbhxye3uD9VANn6etLbowZDNGHrwkWhtw3gFtxMeTyXgP3U1zP5C"',
+        "url" => '""',
+        "user" => '""',
     );
     
     
